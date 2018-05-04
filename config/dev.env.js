fac12dedd47bded5ad2bdea813381734a6561583
dingxiaomeng <15617713649@163.com>
2018-05-04 11:40:59 +0800

    5.4配置文件的修改

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,14 +4,9 @@ const prodEnv = require('./prod.env');
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-<<<<<<< HEAD
    BASE_API: '"http://192.168.0.210:8087"',
   //BASE_API: '"http://120.27.12.205:8005"',
   // BASE_API: '"https://sksj-api.yabei.shop"',
-=======
-   // BASE_API: '"http://192.168.0.210:8087"',
-  BASE_API: '"http://120.27.12.205:8005"',
->>>>>>> 9ba856bb747f328471e0bba0a720c00930513af0
   // BASE_API:'"http://rap2api.taobao.org/app/mock/3968"'
   IMAGE_DOMAIN : '"https://lgf8953.oss-cn-beijing.aliyuncs.com/"',//测试
   //  BASE_API: '"http://sksj-api.yabei.shop"',
