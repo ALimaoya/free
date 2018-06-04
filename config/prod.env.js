6bebeed4231d28cf2cbf51ed3d102fc92b0b9bfc
dingxiaomeng <15617713649@163.com>
2018-06-04 10:05:24 +0800

    6.4环境的改变

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,10 +1,12 @@
 'use strict';
 module.exports = {
   NODE_ENV: '"production"',
-  //BASE_API: '"http://120.27.12.205:8005"',
-   BASE_API: '"https://sksj-api.yabei.shop"',
-  // BASE_API:'"http://rap2api.taobao.org/app/mock/3968"'
-    //IMAGE_DOMAIN : '"http://lgf8953.oss-cn-beijing.aliyuncs.com/"', //测试
- IMAGE_DOMAIN : '"http://yabei.oss-cn-beijing.aliyuncs.com/"',//正式
+  //  正式
+  //  BASE_API: '"https://sksj-api.yabei.shop"',正式
+  //IMAGE_DOMAIN : '"http://yabei.oss-cn-beijing.aliyuncs.com/"',//正式
+
+  // 测试
+  BASE_API: '"http://120.27.12.205:8005"',
+  IMAGE_DOMAIN : '"http://lgf8953.oss-cn-beijing.aliyuncs.com/"', //测试
 
 };
